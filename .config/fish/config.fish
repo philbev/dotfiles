e330961dbd347d0261f36121227b8446078c50d8
philbev <phil@philbev.co.uk>
2023-01-13 15:05:39 +0000

    config.fish: Added LC_ALL environment variable.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     export LESS='-eXRF'
+    export LC_ALL='en_GB.UTF-8'
     export QT_QPA_PLATFORMTHEME=qt5ct
     if not ps ax | grep picom | grep -v grep >/dev/null
         and [ $XDG_CURRENT_DESKTOP != KDE ]   # Plasma has its own compositor.
