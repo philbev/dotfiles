3b989a02836158aed19bb4e472a8f6485d65d754
philbev <phil@philbev.co.uk>
2023-03-01 08:24:19 +0000

    Emacs: Configuration.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,118 +3,182 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(atom-one-dark))
- '(custom-safe-themes
-   '("d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" "b1a691bb67bd8bd85b76998caf2386c9a7b2ac98a116534071364ed6489b695d" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" "9b4ae6aa7581d529e20e5e503208316c5ef4c7005be49fdb06e5d07160b67adc" default))
- '(org-agenda-files '("/home/phil/mylife.org" "~/todo.org"))
+ '(custom-enabled-themes '(tango-dark))
+ '(org-agenda-files '("/home/phil/mylife.org"))
  '(package-selected-packages
-   '(org-bullets lua-mode gruvbox-theme atom-dark-theme atom-one-dark-theme which-key linum-relative markdown-mode org-superstar evil))
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
+   '(which-key use-package org-superstar org-bullets markdown-mode lua-mode linum-relative gruvbox-theme evil atom-one-dark-theme atom-dark-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "JetBrainsMono Nerd Font Mono" :foundry "JB" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "JetBrainsMono Nerd Font Mono" :foundry "JB" :slant normal :weight normal :height 128 :width normal))))
+ '(region ((t (:extend t :background "#CCCCCC")))))
 
-(global-set-key (kbd "C-c l") #'org-store-link)
-(global-set-key (kbd "C-c a") #'org-agenda)
-(global-set-key (kbd "C-c c") #'org-capture)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Emacs Elements
+;; URL: https://youtu.be/eRHLGWajcTk
+;; Last updated 19 February 2023
+;; Thanks to Tim Cross for some suggestions
 
+;; Save History
+(savehist-mode +1)
+(setq savehist-additional-variables '(kill-ring search-ring regexp-search-ring))
 
- (require 'package)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;; Startup
+(setq inhibit-startup-screen t)
+(setq initial-scratch-message
+      ";; Hello world.\n")
+
+;; Size of the starting Window
+(setq initial-frame-alist '((top . 1)
+			    (left . 450)
+			    (width . 101)
+			    (height . 70)))
+
+;; Use `use-package'
+;; This code is not required in Emacs 29 which bundles `use-package'
+(eval-when-compile (add-to-list 'load-path "/home/phil/.emacs.d/use-package")
+		   (require 'use-package))
+
+;; Set the default directory
+(setq default-directory "/home/phil/")
+
+;; Package directory
+(add-to-list 'load-path "/home/phil/.emacs.d/Packages")
+
+;; Basic modes
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(blink-cursor-mode -1)
+(column-number-mode +1)
+(global-visual-line-mode +1)
+(delete-selection-mode +1)
+(save-place-mode +1)
+
+;; Recent files
+(recentf-mode 1)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+;; Melpa
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+(setq package-check-signature nil)
 
-;;(package-refresh-contents)
+;; UTF-8
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8-unix)
+(set-terminal-coding-system 'utf-8-unix)
 
-;; Download Evil
-(unless (package-installed-p 'evil)(require 'org-superstar)
-(add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
-  (package-install 'evil))
+;; Backups
+;; URL: https://sachachua.com/dotemacs/index.html
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq delete-old-versions -1)
+(setq version-control t)
+(setq vc-make-backup-files t)
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
 
-;; Enable Evil
+;; Quickly access dot emacs
+(global-set-key (kbd "C-c i")
+		(lambda()
+		"Edit emacs config file: ~/.emacs.d/init.el"
+		  (interactive)
+		  (find-file "~/.emacs.d/init.el")))
+
+;; Quickly access dot emacs d
+(global-set-key (kbd "C-c e")
+    (lambda()
+      (interactive)
+      (find-file "~/.emacs.d")))
+
+;; Global keys
+;; If you use a window manager be careful of possible key binding clashes
+(setq recenter-positions '(top middle bottom))
+(global-set-key (kbd "C-1") 'kill-this-buffer)
+(global-set-key (kbd "C-<down>") (kbd "C-u 1 C-v"))
+(global-set-key (kbd "C-<up>") (kbd "C-u 1 M-v"))
+(global-set-key [C-tab] 'other-window)
+(global-set-key (kbd "C-c c") 'calendar)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "M-/") #'hippie-expand)
+(global-set-key (kbd "C-x C-j") 'dired-jump)
+(global-set-key (kbd "C-c r") 'remember)
+
+;; dired
+(setq dired-listing-switches "-alt --dired --group-directories-first -h -G")
+
+(add-hook 'dired-mode-hook 'dired-hide-details-mode)
+
+(add-hook 'dired-mode-hook (lambda () (dired-omit-mode)))
+(setq dired-omit-files
+      "\\`[.]?#\\|\\.java\\|snap\\|System\\|\\.ssh\\|\\.gitconfig\\|\\.wget\\|\\.aspell\\|\\.cache\\|\\.lesshst\\|\\.gftp\\|\\.pki\\|\\.gnome\\|VirtualBox\\|master\\.tar\\.gz\\|\\.wine\\|plan9port\\|\\.idm\\|\\.font\\|\\.iso\\|\\.cargo\\|lib\\|amd64\\|\\.gnupg\\|\\.python\\|\\.var\\|\\.local\\|\\`[.][.]?\\'")
+
+
+
+
+
+(setq case-fold-search t)
+
+(setq sentence-end-double-space nil)
+
+;; Browse URLS in text mode
+(global-goto-address-mode +1)
+
+;; FINIS
+
+;; Evil configuration
+(setq evil-want-C-i-jump nil)
 (require 'evil)
-(evil-mode 1)
+(evil-mode t)
 
-(require 'org-superstar)
-(add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
 
-;; Enable tab key to cycle through outlines in org-mode
-(evil-define-key 'normal org-mode-map (kbd "<tab>") #'org-cycle)
-
-;; Lua language support
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((lua . t)))
-
-;; org mode
-(require 'org)
-
-;; Line numbers
-(linum-relative-global-mode)
-
-;; Which-key
-;;(add-to-list 'load-path "path/to/which-key.el")
-;;(add-to-list 'load-path "~/.emacs.d/elpa/which-key-3.6.0/which-key.el")
-;;(require 'which-key)
+;; Configuration of package 'which-key'
+(add-to-list 'load-path "path/to/which-key.el")
+(require 'which-key)
 (which-key-mode)
 
-(setq org-startup-folded t)
+ '(cursor ((t (:background "PeachPuff"))))
+ '(hl-line ((t (:background "#454545"))n))
 
-(hl-line-mode)
-(setq inhibit-startup-message t ; Two variables in one call
-      visible-bell nil)
+(linum-mode t)
+(require 'linum-relative)
+(linum-relative-mode t)
 
-;;(load-theme 'atom-one-dark)
-(load-theme 'tango-dark)
-;;(load-theme 'gruvbox)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
-(hl-line-mode 1)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;        org-mode configuration            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-set-key (kbd "C-c a") 'org-agenda)
+;;(define-key org-mode-map (kbd "<C-left>") 'org-shiftleft)
+;;(define-key org-mode-map (kbd "<C-right>") 'org-shiftright)
+(setq org-icalendar-include-todo t)
+;;(setq org-default-notes-file (concat org-directory "/notes.org"))
+(define-key global-map "\C-cc" 'org-capture)
+(add-hook 'org-mode-hook (auto-fill-mode t))
 
-(defun dw/org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode 1)
-  (auto-fill-mode 0)
-  (visual-line-mode 1)
-  (setq evil-auto-indent nil))
+;;(require 'org-journal)
+;;(setq org-capture-templates
+;;      '(("t" "Todo" entry (file+headline "~/Dropbox/Apps/Emacs/tasks.org" "Tasks")
+;;             "* TODO \n  - %?\n  %i\n  %a")
+;;    ("c" "Computing Stuff" entry (file+headline "~/Dropbox/Apps/Emacs/computer_stuff.org" "New Captures")
+;;                      "* TODO \n  - %?\n  %i\n  %a")
+;;    ("d" "Desktop Notes" entry (file+headline "~/Dropbox/Apps/Emacs/desktop_notes.org" "New Captures")
+;;                      "* TODO \n  - %?\n  %i\n  %a")
+;;    ("e" "Emacs" entry (file+headline "~/Dropbox/Apps/Emacs/emacs.org" "New Captures")
+;;                      "* TODO \n  - %?\n  %i\n  %a")
+;;    ("g" "GDB" entry (file+headline "~/Dropbox/Apps/Emacs/gdb.org" "New Capture")
+;;                      "* TODO \n  - %?\n  %i\n  %a")
+;;        ("j" "Journal" entry (file+datetree "~/Dropbox/Apps/Emacs/journal.org")
+;;         "* \n  - %?\nEntered on %U\n  %i\n  %a")
+;;    ("m" "Mylife" entry (file+headline "~/Dropbox/Apps/Emacs/mylife.org" "New Captures")
+;;                      "* TODO \n  - %?\n  %i\n  %a")
+;;    ("v" "Vim" entry (file+headline "~/Dropbox/Apps/Emacs/vim.org" "New Capture")
+;;                      "* TODO \n  - %?\n  %i\n  %a")))
 
-(use-package org
-  :hook (org-mode . dw/org-mode-setup)
-  :config
-  (setq org-ellipsis " ▾"
-        org-hide-emphasis-markers t))
-
-(use-package org-bullets
-  :after org
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
-;; Replace list hyphen with dot
-(font-lock-add-keywords 'org-mode
-                        '(("^ *\\([-]\\) "
-                          (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-(dolist (face '((org-level-1 . 1.2)
-                (org-level-2 . 1.1)
-                (org-level-3 . 1.05)
-                (org-level-4 . 1.0)
-                (org-level-5 . 1.1)
-                (org-level-6 . 1.1)
-                (org-level-7 . 1.1)
-                (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
-
-;; Make sure org-indent face is available
-(require 'org-indent)
-
-;; Ensure that anything that should be fixed-pitch in Org files appears that way
-(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-(set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
-(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+;; Some nice bullets for org mode.
+(require 'org-superstar)
+   (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
