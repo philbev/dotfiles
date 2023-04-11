b7fb1d473c81eb9a54b4cd0774c67a1b0fe69843
philbev <phil@philbev.co.uk>
2023-04-11 10:53:12 +0100

    wezterm.lua: Updated to latest version.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,20 +3,21 @@
 
 local wezterm = require 'wezterm'
 local act = wezterm.action
+local config = {}
 
-return {
-    default_prog = { '/usr/bin/fish'},
-    font = wezterm.font('JetBrains Mono', { weight = 'Regular', italic = false }),
-    --harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },  -- Disables ligatures <= >= ->
-    font_size = 14.0,
-    color_scheme = 'Gruvbox dark, pale (base16)',
-    hide_tab_bar_if_only_one_tab = true,
-    use_fancy_tab_bar = true,
-    force_reverse_video_cursor = false,
-    window_background_opacity = 0.87,
-    window_decorations = 'RESIZE',
-    keys = {
+    config.default_prog = { '/usr/bin/fish'}
+    config.font = wezterm.font('JetBrains Mono', { weight = 'Regular', italic = false })
+    -- config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }  -- Disables ligatures <= >= ->
+    config.font_size = 14.0
+    config.color_scheme = 'Gruvbox dark, pale (base16)'
+    config.hide_tab_bar_if_only_one_tab = true
+    config.use_fancy_tab_bar = true
+    config.force_reverse_video_cursor = false
+    config.window_background_opacity = 0.87
+    config.window_decorations = 'RESIZE'
+    config.keys = {
     { key = 'UpArrow', mods = 'SHIFT', action = act.ScrollByLine(-1) },
-    { key = 'DownArrow', mods = 'SHIFT', action = act.ScrollByLine(1) },
-  },
-}
+    { key = 'DownArrow', mods = 'SHIFT', action = act.ScrollByLine(1) }
+  }
+
+return config
