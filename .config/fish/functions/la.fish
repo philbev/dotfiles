76457a83fe611f59ea7d7ccf4834c61817d0548d
philbev <phil@philbev.co.uk>
2023-06-05 07:42:05 +0100

    Fish configuration.

--- .config/fish/functions/la.fish
+++ .config/fish/functions/la.fish
@@ -1,3 +1,3 @@
-function la --wraps='exa -laghF --icons --git --group-directories-first --color=always $argv | less' --description 'alias la=exa -laghF --icons --git --group-directories-first --color=always $argv | less'
-  exa -laghF --icons --git --group-directories-first --color=always $argv | less; 
+function la --wraps='exa -laghF --icons --git --group-directories-first --color=always $argv | less' --description 'alias la=exa -laghF --icons --git --group-directories-first --color=always $argv | less -r'
+  exa -laghF --icons --git --group-directories-first --color=always $argv | less -r; 
 end
